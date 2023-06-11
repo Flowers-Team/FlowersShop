d4b91d6e0e6b9b3da7f7f18ffccfebca3243cbdd
bot100 <andrezel02@gmail.com>
2023-06-11 17:02:55 +0300

    version 1.7

---
+++ src/components/shopping-cart/shopping-cart.js
@@ -0,0 +1,6 @@
+const shopping_cart_icon = document.querySelector(".shopping-cart-icon");
+const shopping_cart = document.querySelector(".shopping-cart");
+
+shopping_cart_icon.addEventListener("click", () => {
+  shopping_cart.style.display = "block";
+});
